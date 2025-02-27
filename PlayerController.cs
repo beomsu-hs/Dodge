c15ce9376739ce3732083ca0544014f067419000
beomsu-hs <kjt132kmsa@gmail.com>
2025-02-27 10:08:08 +0900

    PlayerController.cs

---
+++ PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    public float speed = 8f;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
+        /*
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            // 위쪽 방향키 입력이 감지된 경우 z 방향 힘 주기 
+            playerRigidbody.AddForce(0f, 0f,speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            // 아래쪽 방향키 입력이 감지된 경우 -z 방향 힘 주기 
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            // 오른쪽 방향키 입력이 감지된 경우 x 방향 힘 주기 
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            // 왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기 
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
+        */
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+        // 씬에 존재하는 GameManager 타입의 오브젝트를 찾아서 가져오기
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        // 가져온 GameManager 오브젝트의 EndGameO 메서드 실행 
+        gameManager.EndGame();
+    }
+}
